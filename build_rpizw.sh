af21ba36ec75d6f463ab54feac8782b19554da99
Tamas Kelemen <kelementa@gmail.com>
2021-09-20 18:00:46 +0200

    .

--- build_rpizw.sh
+++ build_rpizw.sh
@@ -14,8 +14,8 @@ downloadKernel() {
 	else
 		# if the directory does not exist
 		printf "${MAGENTA}Downloading kernel source...${NORMAL}\n"
-		#git clone --depth=1 https://github.com/raspberrypi/linux $KERNELDIR
-		git clone https://github.com/orangepi-xunlong/OrangePiRDA_external.git $KERNELDIR
+		git clone --depth=1 https://github.com/raspberrypi/linux $KERNELDIR
+		#git clone https://github.com/orangepi-xunlong/OrangePiRDA_external.git $KERNELDIR
 	fi
 }
 
@@ -36,119 +36,49 @@ downloadBootFiles() {
 	fi
 }
 
+downloadRootFS() {
+	echo .
+}
+
 createConfigTXT() {
-	if [ -d "$BOOTFSDIR" ]
-	then
-		# if the bootfs directory exists
-		rm -rf $BOOTFSDIR/config.txt
-		printf "${RED}Creating config.txt...${NORMAL}\n"
-		cat << EOF >> $BOOTFSDIR/config.txt
-		kernel=zImage
-		enable_uart=1
-		device_tree=bcm2835-rpi-zero-w.dtb
-		dtoverlay=disable-bt
+	printf "${RED}Creating config.txt...${NORMAL}\n"
+	cat << EOF >> $BOOTFSDIR/config.txt
+	kernel=zImage
+	enable_uart=1
+	device_tree=bcm2835-rpi-zero-w.dtb
+	dtoverlay=disable-bt
+
 EOF
-	else
-		printf "${RED}Config.txt already exists.${NORMAL}\n"
-	fi
 }
+
 createCmdLineTXT() {
-	if [ -d "$BOOTFSDIR" ]
-	then
-		# if the bootfs directory exists
-		printf "${RED}Creating cmdline.txt...${NORMAL}\n"
-		cat << EOF >> $RPI_BOOT/cmdline.txt
-		console=tty1 console=serial0,115200 root=/dev/mmcblk0p2 rootfstype=ext4 rootwait
+	printf "${RED}Creating cmdline.txt...${NORMAL}\n"
+	cat << EOF >> $BOOTFSDIR/cmdline.txt
+	console=tty1 console=serial0,115200 root=/dev/mmcblk0p2 rootfstype=ext4 rootwait
 EOF
-	else
-
-	fi
 }
-
-
-
-
-
-
-prepare() {
-	# ---
-	printf "${RED}Removing build directory...${NORMAL}\n"
-	rm -rf /home/kelement/rpi
-	printf "${RED}Creating build directory...${NORMAL}\n"
-	mkdir -p /home/kelement/rpi
-	printf "${RED}Setting up variables...${NORMAL}\n"
-	export RPI_DIR=/home/kelement/rpi
-	export RPI_BOOT=$RPI_DIR/boot
-	export RPI_ROOT=$RPI_DIR/root
-	export RPI_KERNEL=$RPI_DIR/linux
-	printf "${RED}Extracting boot files...${NORMAL}\n"
-	cp ~/rpi_boot_files.tar.gz $RPI_DIR
-	mkdir -p $RPI_BOOT
-	mkdir -p $RPI_ROOT
-	tar xvzf $RPI_DIR/rpi_boot_files.tar.gz -C $RPI_BOOT/
-}
-
 
 kernelBuild() {
-	export RPI_KERNEL=/home/kelement/rpi/linux
-	git clone --depth=1 https://github.com/raspberrypi/linux $RPI_KERNEL
-	cd $RPI_KERNEL
+	# building
+	cd $KERNELDIR
 	KERNEL=kernel
 	make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- bcmrpi_defconfig
-	make -j4 ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- zImage modules dtbs
+	make -j12 ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- zImage modules dtbs
 }
 
 copyKernelFiles() {
-printf "${RED}Copiing kernel files to boot dir...${NORMAL}\n"
-export RPI_DIR=/home/kelement/rpi
-export RPI_BOOT=$RPI_DIR/boot
-export RPI_ROOT=$RPI_DIR/root
-export RPI_KERNEL=$RPI_DIR/linux
-mkdir $RPI_BOOT/overlays
-cp $RPI_KERNEL/arch/arm/boot/zImage $RPI_BOOT/
-cp $RPI_KERNEL/arch/arm/boot/dts/bcm2835-rpi-zero-w.dtb $RPI_BOOT/
-cp $RPI_KERNEL/arch/arm/boot/dts/overlays/disable-bt.dtbo $RPI_BOOT/overlays
-}
-
-createFiles() {
-export RPI_DIR=/home/kelement/rpi
-export RPI_BOOT=$RPI_DIR/boot
-export RPI_ROOT=$RPI_DIR/root
-export RPI_KERNEL=$RPI_DIR/linux
-printf "${RED}Creating config.txt...${NORMAL}\n"
-cat << EOF >> $RPI_BOOT/config.txt
-# Use the Linux Kernel we compiled earlier.
-kernel=zImage
-
-# Enable UART so we can use a TTL cable.
-enable_uart=1
-
-# Use the appropriate DTB for our device.
-device_tree=bcm2835-rpi-zero-w.dtb
-
-# Disable Bluetooth via device tree overlay.
-# It's a complicated explanation that you
-# can read about here: https://youtu.be/68jbiuf27AY?t=431
-# IF YOU SKIP THIS STEP, your serial connection will not
-# work correctly.
-dtoverlay=disable-bt
-EOF
-
-printf "${RED}Creating cmdline.txt...${NORMAL}\n"
-cat << EOF >> $RPI_BOOT/cmdline.txt
-console=tty1 console=serial0,115200 root=/dev/mmcblk0p2 rootfstype=ext4 rootwait
-EOF
+	printf "${CYAN}Copiing kernel files to boot dir...${NORMAL}\n"
+	mkdir $BOOTFSDIR/overlays
+	cp $KERNELDIR/arch/arm/boot/zImage $BOOTFSDIR/
+	cp $KERNELDIR/arch/arm/boot/dts/bcm2835-rpi-zero-w.dtb $BOOTFSDIR/	
+	cp $KERNELDIR/arch/arm/boot/dts/overlays/disable-bt.dtbo $BOOTFSDIR/overlays
 }
 
 installModules() {
-export RPI_DIR=/home/kelement/rpi
-export RPI_BOOT=$RPI_DIR/boot
-export RPI_ROOT=$RPI_DIR/root
-export RPI_KERNEL=$RPI_DIR/linux
-printf "${RED}Installing kernel modules...${NORMAL}\n"
-# install modules
-cd $RPI_KERNEL
-echo $pass | sudo -S env PATH=$PATH make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- INSTALL_MOD_PATH=$RPI_ROOT modules_install
+	printf "${RED}Installing kernel modules...${NORMAL}\n"
+	# install modules
+	cd $KERNELDIR
+	echo $pass | sudo -S env PATH=$PATH make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- INSTALL_MOD_PATH=$ROOTFSDIR modules_install
 }
 
 busyBox() {
@@ -216,6 +146,11 @@ compressImage() {
 #printf "${RED}Do not forget to prepare the SD card by execute createFS.sh, then start copyImage.sh to get the compressed folders on the host.${NORMAL}\n"
 
 
-
+# first stage
 #downloadKernel
-downloadBootFiles
+#downloadBootFiles
+#downloadRootFS
+#createConfigTXT
+#createCmdLineTXT
+# second stage
+kernelBuild
