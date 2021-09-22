20b43f9d1f98f33fc3c238b58c2b03798e10edab
Tamas Kelemen <kelementa@gmail.com>
2021-09-22 17:01:01 +0200

    first stable version

--- build_rpizw.sh
+++ build_rpizw.sh
@@ -6,6 +6,7 @@
 source general.sh
 
 downloadKernel() {
+	# stage 1
 	if [ -d "$KERNELDIR" ]
 	then
 		# if the kernel directory exists
@@ -20,6 +21,7 @@ downloadKernel() {
 }
 
 downloadBootFiles() {
+	# stage 1
 	if [ -d "$BOOTFSDIR" ]
 	then
 		# if the bootfs directory exists
@@ -37,21 +39,26 @@ downloadBootFiles() {
 }
 
 downloadRootFS() {
-	echo .
+	# stage 1
+	echo $pass | sudo -S debootstrap --arch=armel --foreign bullseye $ROOTFSDIR
+	echo $pass | sudo -S cp /usr/bin/qemu-arm-static $ROOTFSDIR/usr/bin/
+	echo $pass | sudo -S chroot $ROOTFSDIR /usr/bin/qemu-arm-static /bin/bash -c "/debootstrap/debootstrap --second-stage"
 }
 
+
 createConfigTXT() {
+	# stage 2
 	printf "${RED}Creating config.txt...${NORMAL}\n"
 	cat << EOF >> $BOOTFSDIR/config.txt
 	kernel=zImage
 	enable_uart=1
 	device_tree=bcm2835-rpi-zero-w.dtb
 	dtoverlay=disable-bt
-
 EOF
 }
 
 createCmdLineTXT() {
+	# stage 2
 	printf "${RED}Creating cmdline.txt...${NORMAL}\n"
 	cat << EOF >> $BOOTFSDIR/cmdline.txt
 	console=tty1 console=serial0,115200 root=/dev/mmcblk0p2 rootfstype=ext4 rootwait
@@ -59,7 +66,7 @@ EOF
 }
 
 kernelBuild() {
-	# building
+	# stage 2
 	cd $KERNELDIR
 	KERNEL=kernel
 	make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- bcmrpi_defconfig
@@ -67,14 +74,17 @@ kernelBuild() {
 }
 
 copyKernelFiles() {
+	# stage 2
 	printf "${CYAN}Copiing kernel files to boot dir...${NORMAL}\n"
-	mkdir $BOOTFSDIR/overlays
+	mkdir -p $BOOTFSDIR/overlays
 	cp $KERNELDIR/arch/arm/boot/zImage $BOOTFSDIR/
 	cp $KERNELDIR/arch/arm/boot/dts/bcm2835-rpi-zero-w.dtb $BOOTFSDIR/	
 	cp $KERNELDIR/arch/arm/boot/dts/overlays/disable-bt.dtbo $BOOTFSDIR/overlays
 }
 
+
 installModules() {
+	# stage 2
 	printf "${RED}Installing kernel modules...${NORMAL}\n"
 	# install modules
 	cd $KERNELDIR
@@ -104,53 +114,48 @@ printf "${RED}Install busybox...${NORMAL}\n"
 make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- install
 }
 
-addToRootFS() {
-export RPI_DIR=/home/kelement/rpi
-export RPI_BOOT=$RPI_DIR/boot
-export RPI_ROOT=$RPI_DIR/root
-export RPI_KERNEL=$RPI_DIR/linux
-export RPI_BUSYBOX=$RPI_DIR/busybox
-printf "${RED}Creating FS dirs...${NORMAL}\n"
-mkdir -p $RPI_ROOT/proc
-mkdir -p $RPI_ROOT/sys
-mkdir -p $RPI_ROOT/dev
-mkdir -p $RPI_ROOT/etc
-mkdir -p $RPI_ROOT/etc/init.d
-touch $RPI_ROOT/etc/init.d/rcS
-chmod +x $RPI_ROOT/etc/init.d/rcS
-
-cat << EOF >> $RPI_ROOT/etc/init.d/rcS
-#!/bin/sh
-mount -t proc none /proc
-mount -t sysfs none /sys
-
-echo /sbin/mdev > /proc/sys/kernel/hotplug
-mdev -s
-EOF
+addFilesToRootFS() {
+	# stage 2
+	printf "${RED}Adding files to FS directory...${NORMAL}\n"
+	mkdir -p $ROOTFSDIR/proc
+	mkdir -p $ROOTFSDIR/sys
+	mkdir -p $ROOTFSDIR/dev
+	mkdir -p $ROOTFSDIR/etc
+	#mkdir -p $ROOTFSDIR/etc/init.d
+	#echo $pass | sudo -S touch $ROOTFSDIR/etc/init.d/rcS
+	#echo $pass | sudo -S printf "#!/bin/sh\nmount -t proc none /proc\nmount-t sysfs none /sys\necho /sbin/mdev > /proc/sys/kernel/hotplug\nmdev -s" >>$ROOTFSDIR/etc/init.d/rcS
+	#echo $pass | sudo -S chmod +x $ROOTFSDIR/etc/init.d/rcS
+	
 }
 
 compressImage() {
 	printf "${RED}Compressing the system...${NORMAL}\n"
-	tar cvzf rpiimage.tar.gz $RPI_BOOT/ $RPI_ROOT/
+	echo $pass | sudo -S tar cvzf $ROOTDIR/rpiimage.tar.gz $BOOTFSDIR/ $ROOTFSDIR/
 }
 
-#prepare
-#kernelBuild
-#copyKernelFiles
-#createFiles
-#installModules
-#busyBox
-#addToRootFS
+
 #printf "${GREEN}RPI linux has been built.${NORMAL}\n"
 #printf "\n"
 #printf "${RED}Do not forget to prepare the SD card by execute createFS.sh, then start copyImage.sh to get the compressed folders on the host.${NORMAL}\n"
 
 
-# first stage
-#downloadKernel
-#downloadBootFiles
-#downloadRootFS
-#createConfigTXT
-#createCmdLineTXT
-# second stage
-kernelBuild
+firstStage() {
+	# first stage
+	downloadKernel
+	downloadBootFiles
+	downloadRootFS
+	
+}
+
+secondStage() {
+	# second stage
+	kernelBuild
+	copyKernelFiles
+	installModules
+	createConfigTXT
+	createCmdLineTXT
+	addFilesToRootFS
+	compressImage
+}
+
+firstStage
