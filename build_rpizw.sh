d3f9911b416fdf8a43ea2b12705326d0a446a1af
kelementa <kelementa@gmail.com>
2021-10-06 16:45:11 +0200

    .

--- build_rpizw.sh
+++ build_rpizw.sh
@@ -17,11 +17,11 @@ downloadKernel() {
 	if [ -d "$KERNELDIR" ]
 	then
 		# if the kernel directory exists
-		printf "${RED}The kernel directory exists! Skipping...${NORMAL}\n"
+		printf "${RED}[ The kernel directory exists! Skipping... ]${NORMAL}\n"
 		# permanently stays in - Exit 1
 	else
 		# if the directory does not exist
-		printf "${RED}Downloading kernel source...${NORMAL}\n"
+		printf "${RED}[ Downloading kernel source... ]${NORMAL}\n"
 		mkdir -p $ROOTDIR
 		tar xzf ~/linux.tar.gz -C $ROOTDIR
 		#git clone --depth=1 https://github.com/raspberrypi/linux $KERNELDIR
@@ -33,15 +33,15 @@ downloadBootFiles() {
 	if [ -d "$BOOTFSDIR" ]
 	then
 		# if the bootfs directory exists
-		printf "${RED}The bootfs directory exists! Skipping...${NORMAL}\n"
+		printf "${RED}[ The bootfs directory exists! Skipping... ]${NORMAL}\n"
 		# permanently stays in Exit 1
 	else
 		# if the directory does not exist
-		printf "${RED}Creating bootfs directory...${NORMAL}\n"
+		printf "${RED}[ Creating bootfs directory... ]${NORMAL}\n"
 		mkdir -p $BOOTFSDIR
-		printf "${RED}Copy boot files...${NORMAL}\n"
+		printf "${RED}[ Copy boot files... ]${NORMAL}\n"
 		cp ~/rpi_boot_files.tar.gz $BOOTFSDIR
-		printf "${RED}Extracting boot files...${NORMAL}\n"
+		printf "${RED}[ Extracting boot files... ]${NORMAL}\n"
 		tar xzf ~/rpi_boot_files.tar.gz -C $BOOTFSDIR/
 	fi
 }
@@ -50,34 +50,34 @@ downloadRootFS() {
 	# stage 1
 	if [ -d "$ROOTFSDIR" ]
 	then
-		printf "${RED}The rootfs directory exists! Deleting...${NORMAL}\n"
+		printf "${RED}[ The rootfs directory exists! Deleting... ]${NORMAL}\n"
 		echo $pass | sudo -S rm -rf $ROOTFSDIR
 	fi
-	printf "${RED}Starting debootstrap...${NORMAL}\n"
+	printf "${RED}[ Starting debootstrap... ]${NORMAL}\n"
 	export http_proxy=http://127.0.0.1:8000
 	echo $pass | sudo -S debootstrap --arch=armel --components main,non-free --foreign bullseye $ROOTFSDIR http://127.0.0.1:9999/debian
 	echo $pass | sudo -S cp /usr/bin/qemu-arm-static $ROOTFSDIR/usr/bin/
 	echo $pass | sudo -S chroot $ROOTFSDIR /usr/bin/qemu-arm-static /bin/bash -c "/debootstrap/debootstrap --second-stage"
-	printf "${RED}Installing locales...${NORMAL}\n"
+	printf "${RED}[ Installing locales... ]${NORMAL}\n"
 	echo $pass | sudo -S chroot $ROOTFSDIR /usr/bin/qemu-arm-static /bin/bash -c "apt install -y locales"
-	printf "${RED}Setting up locales...${NORMAL}\n"
+	printf "${RED}[ Setting up locales... ]${NORMAL}\n"
 	echo $pass | sudo -S chroot $ROOTFSDIR /usr/bin/qemu-arm-static /bin/bash -c 'echo "en_US.UTF-8 UTF-8" > /etc/locale.gen'
 	echo $pass | sudo -S chroot rpi/rootfs /usr/bin/qemu-arm-static /bin/bash -c 'printf "LC_CTYPE=\"en_US.UTF-8\"" > /etc/default/locale'
 	echo $pass | sudo -S chroot $ROOTFSDIR /usr/bin/qemu-arm-static /bin/bash -c 'LANG="en_US.UTF-8"'
 	echo $pass | sudo -S chroot $ROOTFSDIR /usr/bin/qemu-arm-static /bin/bash -c 'locale-gen'
 	echo $pass | sudo -S chroot $ROOTFSDIR /usr/bin/qemu-arm-static /bin/bash -c 'export LC_ALL="en_US.UTF-8"'
 
-	printf "${RED}Disabling cert checking temporally...${NORMAL}\n"
+	printf "${RED}[ Disabling cert checking temporally... ]${NORMAL}\n"
 	echo $pass | sudo -S chroot $ROOTFSDIR /usr/bin/qemu-arm-static /bin/bash -c "echo 'Acquire::https::deb.debian.org::Verify-Peer "false";' > /etc/apt/apt.conf.d/99debianorg-cert"
-	printf "${RED}Creating sources.list...${NORMAL}\n"
+	printf "${RED}[ Creating sources.list... ]${NORMAL}\n"
 	echo $pass | sudo -S bash -c 'printf "deb http://127.0.0.1:9999/debian bullseye main non-free" > /etc/apt/sources.list'
-	printf "${RED}Installing software-properites-common...${NORMAL}\n"
+	printf "${RED}[ Installing software-properites-common...${NORMAL}\n"
 	echo $pass | sudo -S chroot $ROOTFSDIR /usr/bin/qemu-arm-static /bin/bash -c "apt install -y software-properties-common"
 	#printf "${RED}Adding non-free repository...${NORMAL}\n"
 	#echo $pass | sudo -S chroot $ROOTFSDIR /usr/bin/qemu-arm-static /bin/bash -c "apt-add-repository non-free"
-	printf "${RED}Updating packages...${NORMAL}\n"
+	printf "${RED}[ Updating packages... ]${NORMAL}\n"
 	echo $pass | sudo -S chroot $ROOTFSDIR /usr/bin/qemu-arm-static /bin/bash -c "apt update"
-	printf "${RED}Installing packages...${NORMAL}\n"
+	printf "${RED}[ Installing packages... ]${NORMAL}\n"
 	echo $pass | sudo -S chroot $ROOTFSDIR /usr/bin/qemu-arm-static /bin/bash -c "DEBIAN_FRONTEND=noninteractive apt install -y keyboard-configuration console-setup wpasupplicant net-tools aptitude ca-certificates crda fake-hwclock gnupg man-db manpages ntp usb-modeswitch ssh wget xz-utils locales firmware-brcm80211"
 	#printf "${RED}Adding non-free...${NORMAL}\n"
 	#echo $pass | sudo -S chroot $ROOTFSDIR /usr/bin/qemu-arm-static /bin/bash -c 'rm /etc/apt/sources.list'
@@ -91,7 +91,7 @@ downloadRootFS() {
 
 createConfigTXT() {
 	# stage 2
-	printf "${RED}Creating config.txt in bootfs...${NORMAL}\n"
+	printf "${RED}[ Creating config.txt in bootfs... ]${NORMAL}\n"
 	cat << EOF >> $BOOTFSDIR/config.txt
 	kernel=zImage
 	enable_uart=1
@@ -106,7 +106,7 @@ EOF
 
 createCmdLineTXT() {
 	# stage 2
-	printf "${RED}Creating cmdline.txt in rootfs...${NORMAL}\n"
+	printf "${RED}[ Creating cmdline.txt in rootfs... ]${NORMAL}\n"
 	cat << EOF >> $BOOTFSDIR/cmdline.txt
 	console=tty1 console=serial0,115200 root=/dev/mmcblk0p2 rootfstype=ext4 rootwait
 EOF
@@ -116,7 +116,7 @@ EOF
 
 kernelBuild() {
 	# stage 2
-	printf "${RED}Building kernel...${NORMAL}\n"
+	printf "${RED}[ Building kernel... ]${NORMAL}\n"
 	cd $KERNELDIR
 	KERNEL=kernel
 	make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- bcmrpi_defconfig
@@ -126,7 +126,7 @@ kernelBuild() {
 
 kernelRebuild() {
 	# stage 2
-	printf "${RED}Rebuilding kernel...${NORMAL}\n"
+	printf "${RED}[ Rebuilding kernel... ]${NORMAL}\n"
 	cd $KERNELDIR
 	KERNEL=kernel
 	make clean
@@ -138,7 +138,7 @@ kernelRebuild() {
 
 copyKernelFiles() {
 	# stage 2
-	printf "${RED}Copiing kernel files to boot dir...${NORMAL}\n"
+	printf "${RED}[ Copiing kernel files to boot dir... ]${NORMAL}\n"
 	mkdir -p $BOOTFSDIR/overlays
 	cp $KERNELDIR/arch/arm/boot/zImage $BOOTFSDIR/
 	cp $KERNELDIR/arch/arm/boot/dts/bcm2835-rpi-zero-w.dtb $BOOTFSDIR/	
@@ -148,7 +148,7 @@ copyKernelFiles() {
 
 installModules() {
 	# stage 2
-	printf "${RED}Installing kernel modules...${NORMAL}\n"
+	printf "${RED}[ Installing kernel modules... ]${NORMAL}\n"
 	# install modules
 	cd $KERNELDIR
 	echo $pass | sudo -S env PATH=$PATH make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- INSTALL_MOD_PATH=$ROOTFSDIR modules_install
@@ -158,14 +158,14 @@ installModules() {
 
 addFilesToRootFS() {
 	# stage 2
-	printf "${RED}Adding files and parameters to rootfs directory...${NORMAL}\n"
+	printf "${RED}[ Adding files and parameters to rootfs directory... ]${NORMAL}\n"
 	mkdir -p $ROOTFSDIR/proc
 	mkdir -p $ROOTFSDIR/sys
 	mkdir -p $ROOTFSDIR/dev
 	mkdir -p $ROOTFSDIR/etc
-	printf "${RED}Setting up root password...${NORMAL}\n"
+	printf "${RED}[ Setting up root password... ]${NORMAL}\n"
 	echo $pass | sudo -S chroot $ROOTFSDIR /usr/bin/qemu-arm-static /bin/bash -c "echo -e \"1234\n1234\" | passwd"
-	printf "${RED}Creating wpa_supplicant config...${NORMAL}\n"
+	printf "${RED}[ Creating wpa_supplicant config... ]${NORMAL}\n"
 	echo $pass | sudo sh -c 'printf "ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\nupdate_config=1\ncountry=HU\nnetwork={\nssid=Bubb_L\npsk augusztus\nkey_mgmt=WPA-PSK\n}\n" > rpi/rootfs/etc/wpa_supplicant/wpa_supplicant.conf'
 	
 	
@@ -173,7 +173,7 @@ addFilesToRootFS() {
 }
 
 compressImage() {
-	printf "${RED}Compressing the built system...${NORMAL}\n"
+	printf "${RED}[ Compressing the built system... ]${NORMAL}\n"
 	echo $pass | sudo -S tar czf $ROOTDIR/rpiimage.tar.gz $BOOTFSDIR/ $ROOTFSDIR/
 }
 
@@ -242,7 +242,7 @@ while getopts hfscra options; do
 			addFilesToRootFS
 			;;
 		*)
-			 printf "${RED}Unknown parameter added ${NORMAL}\n"
+			 printf "${RED}[ Unknown parameter added ]${NORMAL}\n"
 			 exit 1
 		esac
 done
