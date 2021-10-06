ffcfebf81a641e9aca7fd9de70096fc4b6afe813
kelementa <kelementa@gmail.com>
2021-10-06 16:38:36 +0200

    .

--- general.sh
+++ general.sh
@@ -24,4 +24,18 @@ KERNELDIR=${ROOTDIR}/linux
 BOOTFSDIR=${ROOTDIR}/bootfs
 ROOTFSDIR=${ROOTDIR}/rootfs
 
-
+printf "${BLACK}The color is BLACK${NORMAL}\n"
+printf "${RED}The color is RED${NORMAL}\n"
+printf "${GREEN}The color is GREEN${NORMAL}\n"
+printf "${YELLOW}The color is YELLOW${NORMAL}\n"
+printf "${LIME_YELLOW}The color is LIME_YELLOW${NORMAL}\n"
+printf "${POWDER_BLUE}The color is POWDER_BLUE${NORMAL}\n"
+printf "${BLUE}The color is BLUE${NORMAL}\n"
+printf "${MAGENTA}The color is MAGENTA${NORMAL}\n"
+printf "${CYAN}The color is CYAN${NORMAL}\n"
+printf "${WHITE}The color is WHITE${NORMAL}\n"
+printf "${BRIGHT}The color is BRIGHT${NORMAL}\n"
+printf "${NORMAL}The color is NORMAL${NORMAL}\n"
+printf "${BLINK}The color is BLINK${NORMAL}\n"
+printf "${REVERSE}The color is REVERSE${NORMAL}\n"
+printf "${UNDERLINE}The color is UNDERLINE${NORMAL}\n"
